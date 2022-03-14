304b445d07209ba9f67f3d70899499cea43ffe37
Collo <mutaic84@gmail.com>
2022-03-14 23:43:52 +0300

    add variables to hold form data for display and create a prototype method

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-function AddNewPlace(name, country){
+function AddNewPlace(name, country, yearTime){
     this.placeName = name;
     this.countryVisited = country;
+    this.timeOfYear = yearTime;
 }
+
+AddNewPlace.prototype.fullDetails = function() {
+    return this.name + " located in " + this.country;
+}
+
+$(document).ready(function () {
+    $("form").submit(function (e) { 
+        e.preventDefault();
+
+        let placeName = ("input#new-place").val();
+        let countryName = ("input#new-country").val();
+        let yearTime = ("input#new-time").val();
+        let notes = ("input#new-note").val();
+    });
+});
