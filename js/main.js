d4daa701016ff3a85276a188b21cd1a14dd7918c
Collo <mutaic84@gmail.com>
2022-03-15 10:00:12 +0300

    append added placename to ul list

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ function AddNewPlace(name, country, yearTime){
 }
 
 AddNewPlace.prototype.fullDetails = function() {
-    return this.name + " located in " + this.country;
+    return this.placeName + " located in " + this.countryVisited;
 }
 
 $(document).ready(function () {
     $("form").submit(function (e) { 
         e.preventDefault();
 
-        let placeName = ("input#new-place").val();
-        let countryName = ("input#new-country").val();
-        let yearTime = ("input#new-time").val();
-        let notes = ("input#new-note").val();
+        let placeName = $("input#new-place").val();
+        let countryName = $("input#new-country").val();
+        let yearTime = $("input#new-time").val();
+        let notes = $("input#new-note").val();
+
+        let newPlace = new AddNewPlace(placeName, countryName, yearTime);
+
+        $("ul#my-places").append("<li><span class='place'>" + newPlace.fullDetails() + "</span></li>");
     });
 });
